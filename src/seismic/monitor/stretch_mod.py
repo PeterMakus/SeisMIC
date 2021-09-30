14dc64f708d79e0a78069e63b38ae9e7c0f88b11
Peter Makus <makus@gfz-potsdam.de>
2021-09-30 14:29:53 +0200

    changed last change back

--- src/seismic/monitor/stretch_mod.py
+++ src/seismic/monitor/stretch_mod.py
@@ -7,7 +7,7 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Tuesday, 15th June 2021 03:42:14 pm
-Last Modified: Thursday, 30th September 2021 01:48:32 pm
+Last Modified: Thursday, 30th September 2021 02:29:33 pm
 '''
 from typing import List, Tuple
 
@@ -156,9 +156,7 @@ def velocity_change_estimate(
             raise ValueError(
                 'sides = %s not a valid option.' % sides)
 
-        # mask = np.zeros((mat.shape[1],))
-        # Is the shape now correct? This raised an error before.
-        mask = np.zeros(len(tw),)
+        mask = np.zeros((mat.shape[1],))
         mask[ctw] = 1
 
         ref_mask_mat = np.tile(mask, (nstr, 1))
