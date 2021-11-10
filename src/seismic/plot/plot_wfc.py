d771f9e5a037d1d77c24ef0ac3b5ce24e8783805
Peter Makus <makus@gfz-potsdam.de>
2021-11-10 10:20:36 +0100

    save wfc plot

--- src/seismic/plot/plot_wfc.py
+++ src/seismic/plot/plot_wfc.py
@@ -9,7 +9,7 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Monday, 8th November 2021 02:46:15 pm
-Last Modified: Monday, 8th November 2021 02:58:43 pm
+Last Modified: Wednesday, 10th November 2021 10:18:54 am
 '''
 
 from matplotlib import pyplot as plt
@@ -19,9 +19,12 @@ from seismic.plot.plot_utils import set_mpl_params
 
 
 def plot_wfc_bulk(
-        time_lag: np.ndarray, cfreq: np.ndarray, wfc: np.ndarray, title: str):
+    time_lag: np.ndarray, cfreq: np.ndarray, wfc: np.ndarray, title: str,
+        outfile: str):
     set_mpl_params()
     plt.pcolormesh(time_lag, cfreq, wfc)
     plt.xlabel(r"Lag Time [s]")
     plt.ylabel(r"Centre Frequency [Hz]")
     plt.title(title)
+    if outfile:
+        plt.savefig(outfile, format='png', dpi=300)
