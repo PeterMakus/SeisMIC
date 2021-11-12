9fccdd389b974733bde2ec8aa90a62d07fd11f49
Peter Makus <makus@gfz-potsdam.de>
2021-11-12 17:54:08 +0100

    started CorrBulk test

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -7,19 +7,71 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Monday, 31st May 2021 01:50:04 pm
-Last Modified: Friday, 12th November 2021 05:06:07 pm
+Last Modified: Friday, 12th November 2021 05:52:22 pm
 '''
 
 import unittest
 from unittest import mock
-from unittest.case import TestCase
 import warnings
+from copy import deepcopy
 
 import numpy as np
 from obspy import read, read_inventory, Stream
 from obspy.core.utcdatetime import UTCDateTime
 
 from seismic.correlate import stream
+
+
+class TestCorrBulk(unittest.TestCase):
+    def setUp(self):
+        st = read()
+        A = np.zeros((st.count(), st[0].stats.npts))
+        statl = []
+        keys = [
+            'stla', 'stlo', 'stel', 'evla', 'evlo', 'evel', 'dist',
+            'baz', 'az']
+        for k in keys:
+            st[0].stats[k] = 0
+        stats = stream.CorrStats(st[0].stats)
+        for ii, tr in enumerate(st):
+            stats.corr_start += 5
+            A[ii] = tr.data
+            statl.append(stats)
+        self.cb = stream.CorrBulk(A, statlist=statl)
+        np.testing.assert_array_equal(A, self.cb.data)
+
+    def test_setup_no_stats(self):
+        cb = stream.CorrBulk(np.zeros((5, 5)))
+        self.assertEqual(cb.stats.ntrcs, 5)
+        self.assertEqual(cb.stats.npts, 5)
+        self.assertEqual(cb.stats.processing_bulk, [])
+        self.assertIsNone(cb.ref_trc)
+        np.testing.assert_array_equal(np.zeros((5, 5)), cb.data)
+
+    def test_setup_ex_stats(self):
+        cb = stream.CorrBulk(np.zeros((5, 5)), stats=deepcopy(self.cb.stats))
+        for v0, v1 in zip(cb.stats.values(), self.cb.stats.values()):
+            self.assertEqual(v0, v1)
+        np.testing.assert_array_equal(cb.data, np.zeros((5, 5)))
+
+    @mock.patch('seismic.correlate.stream.pcp.corr_mat_normalize')
+    def test_normalize(self, cmn_mock):
+        cb = self.cb.copy()
+        cb.normalize(0, 0, 'blatest')
+        cmn_mock.assert_called_once_with(
+            mock.ANY, cb.stats, 0, 0, 'blatest')
+        self.assertIn(
+            'normalize; normtype: blatest, starttime: 0, endtime: 0',
+            cb.stats.processing_bulk)
+
+    def test_copy(self):
+        cb = self.cb.copy()
+        cb.data += 25
+        cb.stats['eg'] = 1
+        np.testing.assert_array_almost_equal(
+            cb.data-self.cb.data, 25*np.ones_like(cb.data))
+        with self.assertRaises(KeyError):
+            print(self.cb.stats['eg'])
 
 
 class TestCorrStats(unittest.TestCase):
