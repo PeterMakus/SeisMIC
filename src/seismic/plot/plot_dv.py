fixed sign error in scatter plot plot_dv

src/seismic/plot/plot_dv.py
@@ -8,7 +8,7 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Friday, 16th July 2021 02:30:02 pm
-Last Modified: Wednesday, 6th April 2022 03:57:10 pm
+Last Modified: Thursday, 7th April 2022 09:43:11 am
 '''
 
 from datetime import datetime
@@ -210,7 +210,7 @@ def plot_dv(
         histt = np.array([t.timestamp for t in dv['stats']['corr_start']])
         histt = np.tile(histt, dv['values'].shape[0])
         histcorrs = np.reshape(dv['corrs'], -1)
-        histvalues = np.reshape(dv['values'], -1)
+        histvalues = np.reshape(-dv['values'], -1)
         n_bins = np.array(dv['sim_mat'].shape)//50
         # remove nans
         nanmask = ~np.isnan(histcorrs)