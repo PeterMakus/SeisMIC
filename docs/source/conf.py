78bcf0fd8999fdf071f597be599a6428ba41e2c4
PeterMakus <makus@gfz-potsdam.de>
2022-11-07 17:14:53 -0800

    updated version code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, The SeisMIC development team.'
 author = 'The SeisMIC development Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.32'
+release = '0.1.33'
 
 
 # -- General configuration ---------------------------------------------------
