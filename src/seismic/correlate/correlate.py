9bad36480727174a42a0476cbd1574b02aa57bda
Peter Makus <makus@gfz-potsdam.de>
2023-04-04 17:34:13 +0200

    same fix here

--- src/seismic/correlate/correlate.py
+++ src/seismic/correlate/correlate.py
@@ -8,7 +8,7 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Monday, 29th March 2021 07:58:18 am
-Last Modified: Tuesday, 4th April 2023 05:26:33 pm
+Last Modified: Tuesday, 4th April 2023 05:34:04 pm
 '''
 from copy import deepcopy
 from typing import Iterator, List, Tuple
@@ -512,18 +512,22 @@ class Correlator(object):
                 for popi in popindices:
                     del win[popi]
                 # now we have to recompute the combinations
-                if len(popindices):
-                    self.logger.debug('removing redundant data.')
-                    if self.rank == 0:
-                        self.logger.debug('Recalculating combinations...')
-                        self.options['combinations'] = calc_cross_combis(
-                            win, self.ex_dict,
-                            self.options['combination_method'],
-                            rcombis=self.rcombis)
-                    else:
-                        self.options['combinations'] = None
-                    self.options['combinations'] = self.comm.bcast(
-                        self.options['combinations'], root=0)
+                self.logger.debug('removing redundant data.')
+                if self.rank == 0:
+                    self.logger.debug('Recalculating combinations...')
+                    self.options['combinations'] = calc_cross_combis(
+                        win, self.ex_dict,
+                        self.options['combination_method'],
+                        rcombis=self.rcombis)
+                else:
+                    self.options['combinations'] = None
+                self.options['combinations'] = self.comm.bcast(
+                    self.options['combinations'], root=0)
+                if not len(self.options['combinations']):
+                    # no new combinations for this time period
+                    self.logger.info(
+                        f'No new data for times {winstart}-{winend}')
+                    continue
                 # Stream based preprocessing
                 if self.options['preprocess_subdiv']:
                     try:
