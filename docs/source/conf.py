3263ec69fad3094633a1e604cd85210a63156ccb
Peter Makus <makus@gfz-potsdam.de>
2025-02-26 16:41:17 +0100

    reference traces can now be defined by time windows

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = \
     'Peter Makus, Christoph Sens-Schönfelder, and the SeisMIC development Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.12'
+release = '0.6.13'
 
 # -- General configuration ---------------------------------------------------
 
