2f8e3c2e5cf12f388175ea8301eae2ec12ad7d5b
Peter Makus <makus@gfz-potsdam.de>
2026-02-26 13:48:46 +0100

    json dumps fix in monitor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = \
     ' and the SeisMIC development Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.00'
+release = '0.7.1'
 
 # -- General configuration ---------------------------------------------------
 
--- src/seismic/monitor/monitor.py
+++ src/seismic/monitor/monitor.py
@@ -8,7 +8,7 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Thursday, 3rd June 2021 04:15:57 pm
-Last Modified: Monday, 4th August 2025 11:30:40 am
+Last Modified: Thursday, 26th February 2026 01:47:45 pm
 '''
 from copy import deepcopy
 import json
@@ -28,6 +28,7 @@ from tqdm import tqdm
 from seismic.db.corr_hdf5 import CorrelationDataBase, h5_FMTSTR
 from seismic.monitor.dv import DV, read_dv
 from seismic.monitor.wfc import WFC
+from seismic.utils import miic_utils as mu
 from .. import logfactory
 
 parentlogger = logfactory.create_logger()
@@ -97,7 +98,8 @@ class Monitor(logfactory.LoggingMPIBaseClass):
                 pass
             with open(os.path.join(
                     logdir, 'params%s.txt' % tstr), 'w') as file:
-                file.write(json.dumps(opt_dump, indent=1))
+                file.write(
+                    json.dumps(opt_dump, indent=1, default=mu.json_default))
 
         # Find available stations and network
         self.netlist, self.statlist, self.infiles = \
